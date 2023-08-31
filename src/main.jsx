1acd9cbf0c01eaf58cd52030b5fee85741d77ea3
MujuniEli <elijahmujuni@gmail.com>
2023-08-31 07:30:22 +0300

    Added about page route

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import About from './components/About.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <React.StrictMode>
     <Routes>
     <Route path='/' element={<App />}/>
+    <Route path='/about' element={<About />}/>
     </Routes>
   </React.StrictMode>
   </BrowserRouter>,
