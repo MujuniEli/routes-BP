14894b9a875733acbd27f60fd23d187774f15133
MujuniEli <elijahmujuni@gmail.com>
2023-08-31 08:31:32 +0300

    Navigation styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Nav from './components/Nav.jsx'
+
 
 const App = () => {
   return (
     <div>
-        <Nav />
+        
         <h1>Van Life Routes learning</h1>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import About from './pages/About.jsx'
-
+import Nav from './components/Nav.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <React.StrictMode>
+      <Nav />
     <Routes>
     <Route path='/' element={<App />}/>
     <Route path='/about' element={<About />}/>
