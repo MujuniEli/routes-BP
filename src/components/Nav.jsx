7dff7468b95e0c31fb7ed691390f86286a2d50c9
MujuniEli <elijahmujuni@gmail.com>
2023-08-31 08:07:21 +0300

    Added Nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+
+
+const Nav = () => {
+  return (
+    <nav>
+        <img src="../assets/#VANLIFE.png" />
+        <h3>About</h3>
+        <h3>Vans</h3>
+    </nav>
+  )
+}
+
+export default Nav
