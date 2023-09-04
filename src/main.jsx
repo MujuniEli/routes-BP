d1026415bb13aaf58f38d26543132ff56590706d
MujuniEli <elijahmujuni@gmail.com>
2023-09-04 19:18:32 +0300

    Added Layout

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import About from './pages/About.jsx'
 import Home from './pages/Home.jsx'
 import Vans from './pages/Vans.jsx'
 import VanDetails from './pages/VanDetails'
+import Layout from './components/Layout'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -15,10 +16,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <Nav />
     <Routes>
-    <Route path='/' element={<Home />}/>
-    <Route path='/about' element={<About />}/>
-    <Route path='/vans' element={<Vans />}/>
-    <Route path='/vans/:id' element={<VanDetails />}/>
+      <Route element={<Layout />}>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/vans' element={<Vans />}/>
+          <Route path='/vans/:id' element={<VanDetails />}/>
+    </Route>
     </Routes>
   </React.StrictMode>
   </BrowserRouter>,
