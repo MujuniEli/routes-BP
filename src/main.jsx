737af69b0473cc7131247e0357bebe4ba4260b32
MujuniEli <elijahmujuni@gmail.com>
2023-09-06 16:57:57 +0300

    relative path set up

--- src/main.jsx
+++ src/main.jsx
@@ -18,13 +18,13 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <React.StrictMode>
     <Routes>
-      <Route element={<Layout />}>
-          <Route path='/' element={<Home />}/>
-          <Route path='/about' element={<About />}/>
-          <Route path='/vans' element={<Vans />}/>
-          <Route path='/vans/:id' element={<VanDetails />}/>
-              <Route path='/host' element={<Hostlayout />}>
-                <Route path='host' element={<Dashboard />}/>
+      <Route path='/' element={<Layout />}>
+          <Route index element={<Home />}/>
+          <Route path='about' element={<About />}/>
+          <Route path='vans' element={<Vans />}/>
+          <Route path='vans/:id' element={<VanDetails />}/>
+              <Route path='host' element={<Hostlayout />}>
+                <Route index element={<Dashboard />}/>
                 <Route path='reviews' element={<Reviews />}/>
                 <Route path='income' element={<Income />}/>
               </Route>
