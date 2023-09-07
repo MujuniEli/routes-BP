909b5be444871ce7799967ba3d3a7ceae824e283
MujuniEli <elijahmujuni@gmail.com>
2023-09-07 20:29:23 +0300

    Added styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <nav className="nav">
         <Link to="/" className="site-logo">#Vanlife</Link>
         <div className="nav-list">
-            <Link to="/about">About</Link>
-            <Link to="/vans">Vans</Link>
-            <Link to="/host">Host</Link>
+            <NavLink to="/about" className={({isActive}) => isActive ? "active-link" : ""}>About</NavLink>
+            <NavLink to="/vans">Vans</NavLink>
+            <NavLink to="/host">Host</NavLink>
         </div>
     </nav>
   )
