629c7011ef9bed502ef3114e9b646aed83be3f63
MujuniEli <elijahmujuni@gmail.com>
2023-09-07 20:47:03 +0300

    Added nested route styles

--- src/components/Hostlayout.jsx
+++ src/components/Hostlayout.jsx
@@ -1,12 +1,20 @@
 import { NavLink, Outlet } from "react-router-dom";
 
+
+const activeStyle = {
+  fontWeight: "bold",
+  textDecoration: "underline",
+  color: "#161616",
+}
+
 const Hostlayout = () => {
-  return (
+  return (    
+            
              <div className="host-nav">
                 
-                <NavLink to="/host">Dashboard</NavLink>
-                <NavLink to="/host/income">Income</NavLink>
-                <NavLink to="/host/reviews">Reviews</NavLink>
+                <NavLink to="/host" end style={({isActive}) => isActive ? activeStyle : null}>Dashboard</NavLink>
+                <NavLink to="/host/income" style={({isActive}) => isActive ? activeStyle : null}>Income</NavLink>
+                <NavLink to="/host/reviews" style={({isActive}) => isActive ? activeStyle : null}>Reviews</NavLink>
                 <Outlet />
              </div>
   )
