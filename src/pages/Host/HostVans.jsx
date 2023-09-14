8672226d62ff4a42a432d23182c0df7dcedacee3
MujuniEli <elijahmujuni@gmail.com>
2023-09-14 19:45:28 +0300

    Host vans edit

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -22,14 +22,27 @@ const HostVans = () => {
             <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
             <div className="host-van-info">
               <h3>{van.name}</h3>
-              <p>{van.price}/day</p>
+              <p>${van.price}/day</p>
             </div>
             </div>        
         </Link>
   ));
 
   return (
-    <div>HostVans</div>
+            <section>
+                <h1 className="host-vans-title">Your listed Vans</h1>
+                <div className="host-vans-list">
+                    {
+                        hostVans.length > 0 ? (
+                            <section>
+                                {hostVanElements}
+                            </section>
+                        ) : (
+                              <h2>Loading...</h2>
+                        )
+                    }
+                </div>
+            </section>
   )
 }
 
