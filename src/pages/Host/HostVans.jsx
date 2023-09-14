2925de4764ca3b4b4f3a9e1eb1fbafe54fedbf04
MujuniEli <elijahmujuni@gmail.com>
2023-09-14 19:14:39 +0300

    Host van page edits

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,9 +1,28 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 const HostVans = () => {
-  const params = useParams();
-  const [hostVans, setHostVans] = useState(null);
+  
+  const [hostVans, setHostVans] = useState([]);
+
+  useEffect(() => {
+    fetch(`/api/host/vans/`)
+      .then((res) => res.json())
+      .then((data) => setHostVans(data.vans));
+  }, [])
+
+  const hostVanElements = hostVans.map((van) => (
+        <Link
+            to={`/host/vans/${van.id}`}
+            key={van.id}
+            className="host-van-card"
+        >
+          <div className="host-van-single" key={van.id}>
+            
+            </div>        
+        </Link>
+  ));
 
   return (
     <div>HostVans</div>
