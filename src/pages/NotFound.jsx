100aeea3d8a662cea000d65965d524a07f668629
MujuniEli <elijahmujuni@gmail.com>
2023-09-25 08:07:48 +0300

    Added 404 page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+
+
+const NotFound = () => {
+  return (
+            <div>
+                <h3>Sorry! the page you were looking for was not found</h3>
+                <button>Return to Home</button>
+            </div>
+  )
+}
+
+export default NotFound
