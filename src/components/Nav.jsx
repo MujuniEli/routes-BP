4e4efdbcf62fb0ef92540ca3218ec145a24f87d0
MujuniEli <elijahmujuni@gmail.com>
2023-10-17 07:41:26 +0300

    Added clear local storage function

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,19 +2,29 @@ import { NavLink, Link } from "react-router-dom";
 import icon from "../assets/images/login.png";
 
 const Nav = () => {
+
+  const fakeLogOut = () => {
+    localStorage.removeItem("loggedIn")
+  }
+
   return (
     <nav className="nav">
         <Link to="/" className="site-logo">#Vanlife</Link>
         <div className="nav-list">
-            <NavLink to="/about" className={({isActive}) => isActive ? "active-link" : ""}>About</NavLink>
-            <NavLink to="/vans" className={({isActive}) => isActive ? "active-link" : ""}>Vans</NavLink>
-            <NavLink to="/host" className={({isActive}) => isActive ? "active-link" : ""}>Host</NavLink>
-            <Link to="/login" className="login-link">
+            <NavLink 
+            to="about" 
+            className={({isActive}) => isActive ? "active-link" : ""}>
+              About
+            </NavLink>
+            <NavLink to="vans" className={({isActive}) => isActive ? "active-link" : ""}>Vans</NavLink>
+            <NavLink to="host" className={({isActive}) => isActive ? "active-link" : ""}>Host</NavLink>
+            <Link to="login" className="login-link">
                 <img src={icon} 
                 alt="login icon"
                 className="login-icon" 
                 />
             </Link>
+            <button onClick={fakeLogOut}>X</button>
         </div>
     </nav>
   )
