4f6d0daf3df61111b44b40edba63d57be6c94d61
MujuniEli <elijahmujuni@gmail.com>
2023-10-30 21:43:45 +0300

    Added suspense and await modifications

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -2,10 +2,11 @@
 import { Link, useLoaderData, defer, Await } from "react-router-dom";
 import { getHostVans } from "../../server";
 import { requireAuth } from "../../utils";
+import { Suspense } from "react";
 
 
-export  function Loader({ request }) {
-     requireAuth(request)
+export  async function Loader({ request }) {
+     await requireAuth(request)
     return defer({vans: getHostVans()})
 }
 
@@ -14,36 +15,38 @@ const HostVans = () => {
   const hostVansPromise = useLoaderData()
   // const [hostVans, setHostVans] = useState([])
 
-
-  const hostVanElements = hostVans.map(van => (
-        <Link
-            to={van.id}
-            key={van.id}
-            className="host-van-card"
-        >
-          <div className="host-van-single" key={van.id}>
-            <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
-            <div className="host-van-info">
-              <h3>{van.name}</h3>
-              <p>${van.price}/day</p>
-            </div>
-            </div>        
-        </Link>
-  ));
-
   return (
             <section>
                 <h1 className="host-vans-title">Your listed Vans</h1>
+                <Suspense fallback={<h2>Loading Vans....</h2>}>
                 <Await resolve={hostVansPromise.vans}>
                   {vans => {
-
+                              const hostVanElements = vans.map(van => (
+                                <Link
+                                    to={van.id}
+                                    key={van.id}
+                                    className="host-van-card"
+                                >
+                                  <div className="host-van-single" key={van.id}>
+                                    <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                                    <div className="host-van-info">
+                                      <h3>{van.name}</h3>
+                                      <p>${van.price}/day</p>
+                                    </div>
+                                    </div>        
+                                </Link>
+                          ))
+                          return (
+                                    <div className="host-vans-list">
+                                        <section>
+                                            {hostVanElements}
+                                        </section>
+                                    </div>
+                          )
                   }}
-                <div className="host-vans-list">
-                            <section>
-                                {hostVanElements}
-                            </section>
-                </div>
+                
                 </Await>
+                </Suspense>
             </section>
   )
 }
